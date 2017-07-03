8c53d0fce46340a0977231547001542196386ea6
eleves <you@yourdomain.com>
2017-07-03 14:04:03 -0400

    V2

---
+++ ARSimone/GameController.swift
@@ -0,0 +1,18 @@
+//===============================
+import UIKit
+//===============================
+class GameController: UIViewController {
+    
+    //-------------------------------
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //-------------------------------
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    //-------------------------------
+    
+}
+//===============================
---
+++ ARSimone/StartController.swift
@@ -0,0 +1,18 @@
+//===============================
+import UIKit
+//===============================
+class StartController: UIViewController {
+    
+    //-------------------------------
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //-------------------------------
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    //-------------------------------
+
+}
+//===============================
